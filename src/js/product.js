da6f12926f33817c0ab74240aba38ec7c05bd8ad
Glody Mafo <glodymafokis@gmail.com>
2025-09-28 18:44:55 +0200

    dynamic header and footer

--- src/js/product.js
+++ src/js/product.js
@@ -3,6 +3,7 @@ import ProductData from './ProductData.mjs';
 import ProductDetails from './ProductDetails.mjs';
 import { getParam } from './utils.mjs';
 
+
 const dataSource = new ProductData('tents');
 const productID = getParam('product');
 
